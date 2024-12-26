aa83a34a4df342efb23fad79176cd11e0e1c8f35
Naihe <naiheemail@yeah.net>
2024-12-27 00:01:18 +0800

    chore: local store

---
+++ src/composables/system.ts
@@ -0,0 +1,33 @@
+const PREFIX = 'qiniustore:'
+const CONSTANTS = {
+  ACCESS_KEY: PREFIX + 'accessKey',
+  SECRET_KEY: PREFIX + 'secretKey',
+  BUCKET: PREFIX + 'bucket',
+  DIR: PREFIX + 'dir',
+  BUCKETS: PREFIX + 'buckets',
+  BEIAN: PREFIX + 'beian',
+  TINIFY_KEY: PREFIX + 'tinifyKey'
+} as const
+
+type KeyOptions =
+  | 'ACCESS_KEY'
+  | 'SECRET_KEY'
+  | 'BUCKET'
+  | 'DIR'
+  | 'BUCKETS'
+  | 'BEIAN'
+  | 'TINIFY_KEY'
+type ValueOptions = string | number | boolean | string[]
+
+export const db = {
+  // overwrite if it exists.
+  set(key: KeyOptions, v: ValueOptions) {
+    utools.dbCryptoStorage.setItem(CONSTANTS[key], v)
+  },
+  get(key: KeyOptions): ValueOptions {
+    return utools.dbCryptoStorage.getItem(CONSTANTS[key])
+  },
+  del(key: KeyOptions) {
+    utools.dbCryptoStorage.removeItem(CONSTANTS[key])
+  }
+}
